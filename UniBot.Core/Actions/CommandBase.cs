7527399a524a76b097e8d5d9f79adff2c4b50df9
SightRo <tagir00123@gmail.com>
2020-05-24 22:04:23 +0300

    Add CommandBase.

---
+++ UniBot.Core/Actions/CommandBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using UniBot.Core.Abstraction;
+
+namespace UniBot.Core.Actions
+{
+    public abstract class CommandBase : IAction
+    {
+        public abstract string Name { get; }
+        public abstract string? Description { get; }
+
+        public abstract Task Execute(UpdateContext context);
+
+        public virtual bool CanExecute(UpdateContext context)
+            => true;
+    }
+}
