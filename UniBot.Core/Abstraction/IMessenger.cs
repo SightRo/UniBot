057d36d5342c7e30ad6dbfbd4ea4842c6c624203
SightRo <tagir00123@gmail.com>
2020-05-24 22:01:35 +0300

    Add IMessenger.

---
+++ UniBot.Core/Abstraction/IMessenger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniBot.Core.Models;
+using UniBot.Core.Models.Attachments;
+
+namespace UniBot.Core.Abstraction
+{
+    public interface IMessenger
+    {
+        string Name { get; }
+        
+        Task<IEnumerable<long>> SendMessage(long chatId, OutMessage message);
+        Task<bool> DeleteMessage(long chatId, long messageId);
+        Task<bool> EditMessage(long chatId, long messageId, OutMessage message);
+        Task<FileAttachment?> DownloadAttachment(InAttachment attachment);
+        
+        Task<User?> GetUser(long userId);
+        Task<Chat?> GetChat(long chatId);
+    }
+}
