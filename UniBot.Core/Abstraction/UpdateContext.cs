2131d9dc578b250b6ae47a20004ab5fa3ee047dc
SightRo <tagir00123@gmail.com>
2020-05-24 22:00:55 +0300

    Add UpdateContext.

---
+++ UniBot.Core/Abstraction/UpdateContext.cs
@@ -0,0 +1,17 @@
+﻿using UniBot.Core.Models;
+
+namespace UniBot.Core.Abstraction
+{
+    public class UpdateContext
+    {
+        public UpdateContext(IMessenger messenger, User sender, Chat chat, InMessage? message = null)
+            => (Messenger, Chat, Sender, Message) = (messenger, chat, sender, message);
+        
+        public IMessenger Messenger { get; }
+        public Chat Chat { get; }
+        public User Sender { get; }
+        public InMessage? Message { get; }
+        
+        public string Source => Messenger.Name;
+    }
+}
