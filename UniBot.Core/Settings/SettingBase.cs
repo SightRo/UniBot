8e9ab88b6d43dd9265a46c3e028001cc94940cda
SightRo <tagir00123@gmail.com>
2020-05-24 22:01:12 +0300

    Add SettingBase.

---
+++ UniBot.Core/Settings/SettingBase.cs
@@ -0,0 +1,11 @@
+﻿namespace UniBot.Core.Settings
+{
+    // TODO Make validation.
+    public class SettingBase
+    {
+        public bool IsEnabled { get; set; } = true;
+        public string Token { get; set; } = null!;
+        public long BotOwnerId { get; set; } = 0;
+        public long[] BotAdminIds { get; set; } = new long[0];
+    }
+}
