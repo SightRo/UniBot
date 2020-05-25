3832c2a46f11d26591fce75d8d591119d09061f2
SightRo <tagir00123@gmail.com>
2020-05-25 19:32:50 +0300

    Add KeyboardBuilder.

---
+++ UniBot.Core/Helpers/KeyboardBuilder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UniBot.Core.Models.Keyboards;
+
+namespace UniBot.Core.Helpers
+{
+    public class KeyboardBuilder
+    {
+        public KeyboardBuilder(bool isInline)
+            => _isInline = isInline;
+        
+        private Lazy<List<List<ReplyButton>>> _replyButtons = new Lazy<List<List<ReplyButton>>>();
+        private Lazy<List<List<InlineButton>>> _inlineButtons = new Lazy<List<List<InlineButton>>>();
+        private bool _isInline = false;
+        private bool _isOneTime = false;
+        private int _currentLine = 0;
+
+        public KeyboardBuilder AddButton(string text, string? link = null)
+        {
+            switch (_isInline)
+            {
+                case true:
+                    _inlineButtons.Value[_currentLine].Add(new InlineButton(text, link));
+                    break;
+                case false:
+                    if (link != null)
+                        throw new Exception("Replay button have no link");
+                    _replyButtons.Value[_currentLine].Add(new ReplyButton(text));
+                    break;
+            }
+
+            return this;
+        }
+
+        public KeyboardBuilder AddLine()
+        {
+            var count = _isInline ? _inlineButtons.Value[_currentLine].Count : _replyButtons.Value[_currentLine].Count;
+            if (count != 0)
+                _currentLine++;
+
+            return this;
+        }
+
+        public KeyboardBuilder AddButtonAndLine(string text, string? link = null)
+        {
+            AddButton(text, link);
+            AddLine();
+            
+            return this;
+        }
+
+        public KeyboardBuilder SetOneTime(bool isOneTime)
+        {
+            if (_isInline && isOneTime)
+                throw new ArgumentException("Inline keyboard can't be one time");
+
+            _isOneTime = isOneTime;
+
+            return this;
+        }
+
+        public IKeyboard Build()
+        {
+            switch (_isInline)
+            {
+                case true:
+                    return new InlineKeyboard(_inlineButtons.Value);
+                case false:
+                    return new ReplyKeyboard(_replyButtons.Value, _isOneTime);
+            }
+        }
+    }
+}
