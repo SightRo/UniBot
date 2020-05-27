60aea3a97cca9bb9509367303bc4453053f65fa6
SightRo <tagir00123@gmail.com>
2020-05-27 19:03:22 +0300

    Add missing attribute.

---
+++ UniBot.Core/Abstraction/MessengerImplAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UniBot.Core.Abstraction
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class MessengerImplAttribute : Attribute
+    {
+        public MessengerImplAttribute(string name)
+            => Name = name;
+
+        public string Name { get; }
+    }
+}
