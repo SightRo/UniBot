5c393e16ee2269cc043646a00fb893528021bb8c
SightRo <tagir00123@gmail.com>
2020-05-27 19:01:33 +0300

    Add IBot.

---
+++ UniBot.Core/Abstraction/IBot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using UniBot.Core.Actions;
+
+namespace UniBot.Core.Abstraction
+{
+    public interface IBot
+    {
+        ConcurrentDictionary<string, long> Owners { get; }
+        ConcurrentDictionary<string, long[]> Admins { get; }
+        ConcurrentDictionary<string, IAction> Actions { get; }
+        
+        IConfiguration Configuration { get; }
+        
+        Task ProcessUpdate(UpdateContext context);
+    }
+}
