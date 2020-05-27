428aeefb50337c6bdca5f6c49f9fa51f854971a7
SightRo <tagir00123@gmail.com>
2020-05-27 20:23:41 +0300

    Add TryParseCommand in CommandBase.

--- UniBot.Core/Actions/CommandBase.cs
+++ UniBot.Core/Actions/CommandBase.cs
@@ -12,5 +12,19 @@ namespace UniBot.Core.Actions
 
         public virtual bool CanExecute(UpdateContext context)
             => true;
+        
+        public static bool TryParseCommand(string? text, out string commandName)
+        {
+            commandName = string.Empty;
+            if (string.IsNullOrEmpty(text))
+                return false;
+            text = text.Trim();
+
+            if (!text.StartsWith('/') || text.Length <= 1)
+                return false;
+
+            commandName = text.Substring(1, text.Length - 1);
+            return true;
+        }
     }
 }
