b3740b007fcd1d7ff54db5b99a7efb14441e7620
SightRo <tagir00123@gmail.com>
2020-05-27 20:28:40 +0300

    Add Hello Command.

---
+++ UniBot.Example/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using UniBot.Core.Abstraction;
+using UniBot.Core.Actions;
+using UniBot.Core.Models;
+
+namespace UniBot.Example
+{
+    public class Hello : CommandBase
+    {
+        public override string Name { get; } = "Hello";
+        public override string? Description { get; } = "None.";
+        public override async Task Execute(UpdateContext context)
+        {
+            var messenger = context.Messenger;
+            await messenger.SendMessage(context.Chat.Id, new OutMessage("Hello, Bastard!"));
+        }
+    }
+}
