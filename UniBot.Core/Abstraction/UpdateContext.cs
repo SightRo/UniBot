a990e714d8b1ad533a684639e95666f376f9c557
SightRo <tagir00123@gmail.com>
2020-05-27 19:03:47 +0300

    Perfectionism take over again.

--- UniBot.Core/Abstraction/UpdateContext.cs
+++ UniBot.Core/Abstraction/UpdateContext.cs
@@ -4,7 +4,7 @@ namespace UniBot.Core.Abstraction
 {
     public class UpdateContext
     {
-        public UpdateContext(IMessenger messenger, User sender, Chat chat, InMessage? message = null)
+        public UpdateContext(IMessenger messenger, Chat chat, User sender, InMessage? message = null)
             => (Messenger, Chat, Sender, Message) = (messenger, chat, sender, message);
         
         public IMessenger Messenger { get; }
