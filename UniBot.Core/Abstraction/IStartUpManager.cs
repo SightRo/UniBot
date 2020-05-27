f495da6198e1d23616f1b8ee9a5d77a3aac604be
SightRo <tagir00123@gmail.com>
2020-05-27 20:22:46 +0300

    Add IStartUpManager.

---
+++ UniBot.Core/Abstraction/IStartUpManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniBot.Core.Settings;
+
+namespace UniBot.Core.Abstraction
+{
+    public interface IStartUpManager
+    {
+        void Init(IBot bot, IServiceCollection services, out IMessenger messenger, out SettingsBase settings);
+    }
+}
