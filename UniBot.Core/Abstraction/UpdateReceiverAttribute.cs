b914678cfd27ef836fc19a82f3ad568b98dec400
SightRo <tagir00123@gmail.com>
2020-05-27 20:24:13 +0300

    Add UpdateReceiverAttribute.

---
+++ UniBot.Core/Abstraction/UpdateReceiverAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniBot.Core.Abstraction
+{
+    public class UpdateReceiverAttribute : Attribute
+    {
+        public UpdateReceiverAttribute(string name)
+            => Name = name;
+        
+        public string Name { get; }
+    }
+}
