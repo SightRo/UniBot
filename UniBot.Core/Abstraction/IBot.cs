faf2803e834d72fe85b7486667f6d5d549c06e1e
SightRo <tagir00123@gmail.com>
2020-05-28 16:48:43 +0300

    Rework IBot.

--- UniBot.Core/Abstraction/IBot.cs
+++ UniBot.Core/Abstraction/IBot.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using UniBot.Core.Actions;
+using UniBot.Core.Settings;
 
 namespace UniBot.Core.Abstraction
 {
     public interface IBot
     {
-        ConcurrentDictionary<string, long> Owners { get; }
-        ConcurrentDictionary<string, long[]> Admins { get; }
-        ConcurrentDictionary<string, CommandBase> Commands { get; }
-        
-        IConfiguration Configuration { get; }
+        IReadOnlyDictionary<string, long> Owners { get; }
+        IReadOnlyDictionary<string, long[]> Admins { get; }
+        IReadOnlyDictionary<string, CommandBase> Commands { get; }
+        IReadOnlyDictionary<string, IMessenger> Messengers { get; }
+        BotSettings Settings { get; }
         
         void ProcessUpdate(UpdateContext context);
         IMessenger ResolveMessenger(string name);
