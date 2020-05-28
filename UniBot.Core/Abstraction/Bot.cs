630b8e75c1d216b303b71ae50c8b7c7c0b92ed84
SightRo <tagir00123@gmail.com>
2020-05-28 16:50:30 +0300

    Clean up Bot.

--- UniBot.Core/Abstraction/Bot.cs
+++ UniBot.Core/Abstraction/Bot.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,47 +31,8 @@ namespace UniBot.Core.Abstraction
 
         public void ProcessUpdate(UpdateContext context)
         {
-            if (context.Message != null && CommandBase.TryParseCommand(context.Message.Text, out var commnadName))
-                GetCommand(commnadName)?.Execute(context);
-        }
-
-        public Bot Init(IServiceCollection services)
-        {
-            var builder = services.AddControllers();
-            List<IStartUpManager> managers = new List<IStartUpManager>();
-            var assemblies = AppDomain.CurrentDomain
-                                    .GetAssemblies()
-                                    .Where(ass => ass.GetCustomAttribute<MessengerImplAttribute>() != null);
-            
-            foreach (var assembly in assemblies)
-            {
-                builder.AddApplicationPart(assembly);
-                var temp = assembly.GetTypes()
-                                   .Where(x => x.GetInterface(nameof(IStartUpManager)) != null && !x.IsInterface && !x.IsAbstract)
-                                   .Select(x => (IStartUpManager) Activator.CreateInstance(x)!)
-                                   .First();
-                
-                managers.Add(temp);
-            }
-            
-            foreach (var manager in managers)
-            {
-                manager.Init(this, services, out var messenger, out var settings);
-                if (!settings.IsEnabled)
-                    continue;
-            
-                //Owners.TryAdd(messenger.Name, settings.BotOwnerId);
-                //Admins.TryAdd(messenger.Name, settings.BotAdminIds);
-            }
-
-            var Commands = AppDomain.CurrentDomain
-                                    .GetAssemblies()
-                                    .SelectMany(ass => ass.GetTypes())
-                                    .Where(x => string.Compare(typeof(CommandBase).ToString(), x?.BaseType?.ToString() ?? string.Empty) == 0 && !x.IsInterface && !x.IsAbstract)
-                                    .Select(x => (CommandBase) Activator.CreateInstance(x)!)
-                                    .ToDictionary(x => x.Name);
-
-            return this;
+            if (context.Message != null && CommandBase.TryParseCommand(context.Message.Text, out var commandName))
+                GetCommand(commandName)?.Execute(context);
         }
 
         public IAction? GetCommand(string commandName)
@@ -86,50 +47,27 @@ namespace UniBot.Core.Abstraction
             if (Messengers.TryGetValue(name, out var messenger))
                 return messenger;
             else
-                throw new Exception("Messenger is missed.");
+                throw new Exception($"IMessenger {name} is missed.");
         }
-    }
 
-    public static class BotExtension
-    {
-        public static void Init(this IServiceCollection services, IConfiguration config)
+        internal void RegisterAdmins(string messenger, long[] ids)
+            => AddToDictionary(_admins, messenger, ids);
+
+        internal void RegisterMessenger(string messengerName, IMessenger messenger)
+            => AddToDictionary(_messengers, messengerName, messenger);
+
+        internal void RegisterCommand(CommandBase command)
+            => AddToDictionary(_commands, command.Name, command);
+
+        internal void RegisterOwner(string messenger, long id)
+            => AddToDictionary(_owners, messenger, id);
+        
+        private void AddToDictionary<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+            where TKey : notnull
+            where TValue : notnull
         {
-            var builder = services.AddControllers();
-            var bot = new Bot(config);
-            
-            services.AddSingleton<IBot>(bot);
-            List<IStartUpManager> managers = new List<IStartUpManager>();
-            var assemblies = AppDomain.CurrentDomain
-                                      .GetAssemblies()
-                                      .Where(ass => ass.GetCustomAttribute<MessengerImplAttribute>() != null);
-            
-            foreach (var assembly in assemblies)
-            {
-                builder.AddApplicationPart(assembly);
-                var temp = assembly.GetTypes()
-                                   .Where(x => x.GetInterface(nameof(IStartUpManager)) != null && !x.IsInterface && !x.IsAbstract)
-                                   .Select(x => (IStartUpManager) Activator.CreateInstance(x)!)
-                                   .First();
-                
-                managers.Add(temp);
-            }
-            
-            foreach (var manager in managers)
-            {
-                manager.Init(bot, services, out var messenger, out var settings);
-                if (!settings.IsEnabled)
-                    continue;
-            
-                //Owners.TryAdd(messenger.Name, settings.BotOwnerId);
-                //Admins.TryAdd(messenger.Name, settings.BotAdminIds);
-            }
-
-            var Commands = AppDomain.CurrentDomain
-                                    .GetAssemblies()
-                                    .SelectMany(ass => ass.GetTypes())
-                                    .Where(x => string.Compare(typeof(CommandBase).ToString(), x?.BaseType?.ToString() ?? string.Empty) == 0 && !x.IsInterface && !x.IsAbstract)
-                                    .Select(x => (CommandBase) Activator.CreateInstance(x)!)
-                                    .Select(Commands.);
+            if (!dictionary.ContainsKey(key))
+                dictionary.Add(key, value);
         }
     }
 }
