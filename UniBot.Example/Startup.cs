0778c25de33a283e78db8cfc56a33fd7a90d2aef
SightRo <tagir00123@gmail.com>
2020-05-28 19:51:56 +0300

    Final fix. Those one method call and one package could save 3 hours of debugging but they not.

--- UniBot.Example/Startup.cs
+++ UniBot.Example/Startup.cs
@@ -28,7 +28,7 @@ namespace UniBot.Example
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             
             services.AddBot(Configuration)
                     .DetectCommands();
