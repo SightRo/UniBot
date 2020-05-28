549ebc5711944c1934675d4ce2ec793b66db4c88
SightRo <tagir00123@gmail.com>
2020-05-28 16:53:55 +0300

    Add BotSettings.

---
+++ UniBot.Core/Settings/BotSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace UniBot.Core.Settings
+{
+    public class BotSettings
+    {
+        public string Endpoint { get; set; }
+        public Dictionary<string, IConfigurationSection> Messengers { get; set; }
+    }
+}
