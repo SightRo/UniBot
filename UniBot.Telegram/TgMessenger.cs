b1513177064038a3dbf83963c7d300567a7680ec
SightRo <tagir00123@gmail.com>
2020-05-29 07:53:21 +0300

    Clean up TgMessenger.

--- UniBot.Telegram/TgMessenger.cs
+++ UniBot.Telegram/TgMessenger.cs
@@ -19,13 +19,10 @@ using TgChat = Telegram.Bot.Types.Chat;
 
 namespace UniBot.Telegram
 {
-    // Doesn't interact with anything.
-    // No way to get needed dependencies.            
-    // So doesn't work for now because of reasons above.
     public class TgMessenger : IMessenger
     {
-        private ITelegramBotClient _api = null!;
-        private TgSettings _settings = null!;
+        private readonly ITelegramBotClient _api;
+        private readonly TgSettings _settings;
         public string Name => Constants.Name;
 
         public TgMessenger(ITelegramBotClient api, TgSettings settings)
@@ -141,104 +138,6 @@ namespace UniBot.Telegram
                 Name = chat.Title ?? chat.Username,
                 Owner = ownerId,
                 IsUserConversation = chat.Type == ChatType.Private,
-                MessengerSource = Name
-            };
-        }
-
-        private async Task<InMessage?> ConvertMessage(TgMessage? message)
-        {
-            if (message == null) return null;
-
-            //TODO Forwaded messages is missing
-            return new InMessage
-            {
-                Id = message.MessageId,
-                Date = message.Date,
-                Sender = ConvertUser(message.From),
-                Chat = await ConvertChat(message.Chat, message),
-                Text = message.Text ?? message.Caption,
-                Reply = await ConvertMessage(message.ReplyToMessage),
-                Attachments = ExtractAttachments(message),
-                MessengerSource = Name
-            };
-        }
-
-        private InAttachment[] ExtractAttachments(TgMessage message)
-        {
-            switch (message.Type)
-            {
-                case MessageType.Photo:
-                    return new[]
-                    {
-                        new InAttachment(message.Photo.Last().FileId, AttachmentType.Photo, message.Photo.Last(),
-                            Name, null)
-                    };
-                case MessageType.Audio:
-                    return new[]
-                    {
-                        new InAttachment(message.Audio.FileId, AttachmentType.Audio, message.Audio, Name, null)
-                    };
-                case MessageType.Video:
-                    return new[]
-                    {
-                        new InAttachment(message.Video.FileId, AttachmentType.Video, message.Video, Name, null)
-                    };
-                case MessageType.Voice:
-                    return new[]
-                    {
-                        new InAttachment(message.Voice.FileId, AttachmentType.Voice, message.Voice, Name, null)
-                    };
-                case MessageType.Document:
-                    return new[]
-                    {
-                        new InAttachment(message.Document.FileId, AttachmentType.Document, message.Document, Name,
-                            null)
-                    };
-                case MessageType.Sticker:
-                    return new[]
-                    {
-                        new InAttachment(message.Sticker.FileId, AttachmentType.Sticker, message.Sticker, Name,
-                            null)
-                    };
-                case MessageType.VideoNote:
-                    return new[]
-                    {
-                        new InAttachment(message.VideoNote.FileId, AttachmentType.Video, message.VideoNote, Name,
-                            null)
-                    };
-                default:
-                    return new InAttachment[0];
-            }
-        }
-
-        private User ConvertUser(TgUser user)
-        {
-            return new User
-            {
-                Id = user.Id,
-                Username = user.Username,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                IsHuman = !user.IsBot,
-                MessengerSource = Name
-            };
-        }
-
-        private async Task<Chat> ConvertChat(TgChat chat, TgMessage message)
-        {
-            var isUserConversation = chat.Type == ChatType.Private;
-            long owner = isUserConversation
-                ? message.From.Id
-                : (await _api.GetChatAdministratorsAsync(chat.Id))
-                  .First(u => u.Status == ChatMemberStatus.Creator)
-                  .User.Id;
-
-            return new Chat
-            {
-                Id = chat.Id,
-                Name = chat.Title ?? chat.Username,
-                Owner = owner,
-                IsUserConversation = isUserConversation,
                 MessengerSource = Name
             };
         }
