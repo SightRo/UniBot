c2ad15cbe60e6a73b4b9092ce2f0b474cf2cefce
SightRo <tagir00123@gmail.com>
2020-10-24 16:32:49 +0300

    Add one more awaiting

--- UniBot.Telegram/TgReceiver.cs
+++ UniBot.Telegram/TgReceiver.cs
@@ -29,7 +29,7 @@ namespace UniBot.Telegram
             // Dangerous thing
             // Can skip message if context couldn't be created
             if (context != null)
-                _bot.ProcessUpdate(context);
+                await _bot.ProcessUpdate(context);
             
             return Ok();
         }
