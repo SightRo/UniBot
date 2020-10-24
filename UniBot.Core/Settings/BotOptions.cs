2f5823fea6dd5f84354cc10e7c58e98e232c9984
SightRo <tagir00123@gmail.com>
2020-10-24 17:54:06 +0300

    Fix2

--- UniBot.Core/Settings/BotOptions.cs
+++ UniBot.Core/Settings/BotOptions.cs
@@ -6,7 +6,7 @@ namespace UniBot.Core.Settings
     public class BotOptions
     {
         public string Endpoint { get; set; }
-        public ExecutingOptions ExecutingOptions { get; set; }
+        public ExecutingOptions ExecutingOptions { get; set; } = new ExecutingOptions();
         public Dictionary<string, IConfigurationSection> MessengerOptions { get; set; }
     }
 }
